5dcee4364b77d38748c00958d41f4a8e08d16174
Madhu Priya K B <madhupriyakb2006@gmail.com>
2026-05-30 11:00:37 +0530

    fix CORS fir Vercel domain

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://task-manager-black.vercel.app",
+  "https://task-manager-black-xi.vercel.app",
   /\.vercel\.app$/,
 ];
 
